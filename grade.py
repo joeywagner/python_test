138db61c8c53e1f513788836a0930b20b7bcc96a
Joe <joeywagner@gmail.com>
2015-06-14 13:34:14 -0700

    all

--- grade.py
+++ grade.py
@@ -1,9 +1,22 @@
 try:
-	grade = float("Enter grade:")
+	grade = raw_input("Enter grade:")
+	gradef = float(grade)
 
 except:
-	print("Enter a grade that falls within the range between 0.0-1.0")
+	print("Enter a grade that is a number and falls within the range between 0.0-1.0")
 	quit()
 
-if grade >= 0.9
+if 0.9 <= gradef <= 1.0:
 	print "A"
+if 0.8 <= gradef < 0.9:
+	print "B"
+if 0.7 <= gradef < 0.8:
+	print "C"
+if 0.6 <= gradef < 0.7:
+	print "D"
+if 0.0 <= gradef < 0.6:
+	print "F"
+elif gradef > 1.0:
+	print"The grade is too high."
+elif gradef < 0:
+	print "The grade is too low."
